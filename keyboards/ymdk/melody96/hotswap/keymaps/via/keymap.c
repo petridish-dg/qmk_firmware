417c0b2dd891e0eb5a2b2b41cb11d0574aa3ef2c
petridish-dg <74042416+petridish-dg@users.noreply.github.com>
2024-07-26 11:52:08 -0700

    Update keymap.c

--- keyboards/ymdk/melody96/hotswap/keymaps/via/keymap.c
+++ keyboards/ymdk/melody96/hotswap/keymaps/via/keymap.c
@@ -3,6 +3,16 @@
 
 #include QMK_KEYBOARD_H
 
+#ifdef KEY_CANCELLATION_ENABLE
+const key_cancellation_t PROGMEM key_cancellation_list[] = {
+    // on key down
+    //       |    key to be released
+    //       |     |
+    [0] = {KC_D, KC_A},
+    [1] = {KC_A, KC_D}
+};
+#endif
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_96_iso(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR, KC_HOME, KC_END,  KC_PGUP, KC_PGDN, KC_DEL,
